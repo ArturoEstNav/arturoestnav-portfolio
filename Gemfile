148b633aea80bc36d64ad76d27b4cced5ef7f109
Arturo Estrada <arturoestradanav@gmail.com>
2020-10-22 23:54:22 -0500

    added mail form gems, controllers and views

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'devise'
 # Cloudinary for pictures
 gem 'cloudinary'
 
+# Email platform
+gem 'mail_form'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,28 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = "Something went wrong,
+                          please email me at
+                          contact@arturoestnav.com"
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name,
+                                    :email,
+                                    :nickname,
+                                    :message)
+
+  end
+end
