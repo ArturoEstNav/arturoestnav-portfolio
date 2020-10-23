32e36c7e1184e662ede328782b0440d222ccc751
Arturo Estrada <arturoestradanav@gmail.com>
2020-10-23 18:35:51 -0500

    carousel elements brought in not yet implemented

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,28 @@
+class ContactsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = "Something went wrong,
+                          please email me at
+                          contact@arturoestnav.com"
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name,
+                                    :email,
+                                    :message,
+                                    :nickname)
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,36 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+<<<<<<< Updated upstream
 end
+=======
+
+  # historic
+    config.action_mailer.default_url_options = { host: "https://arturoestradanav-portfolio.herokuapp.com/" }
+    config.action_mailer.delivery_method = :smtp
+
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'heroku.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+
+
+#     config.action_mailer.default_url_options = { host: "http://www.arturoestnav.com/" }
+#   config.action_mailer.delivery_method = :smtp
+
+#   ActionMailer::Base.smtp_settings = {
+#     :user_name => ENV['SENDGRID_USERNAME'],
+#     :password => ENV['SENDGRID_PASSWORD'],
+#     :domain => 'arturoestnav.com',
+#     :address => 'smtp.sendgrid.net',
+#     :port => 587,
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+#   }
+# end
+>>>>>>> Stashed changes
