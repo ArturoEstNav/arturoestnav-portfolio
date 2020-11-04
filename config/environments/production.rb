4a34d5dad0e156b8c5253350429d7899ff31cfbf
Arturo Estrada <arturoestradanav@gmail.com>
2020-11-03 21:07:20 -0600

    contact form test view and settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,5 +114,30 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: "https://arturoestradanav-portfolio.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'arturoestradanav-portfolio.herokuapp.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
+
+
+# config.action_mailer.default_url_options = { :host => 'new_app_name.herokuapp.com' }
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.perform_deliveries = true
+# config.action_mailer.raise_delivery_errors = false
+# config.action_mailer.default :charset => "utf-8"
+# config.action_mailer.smtp_settings = {
+#   address:              'smtp.gmail.com',
+#   port:                 587,
+#   domain:               'new_app_name.herokuapp.com',
+#   user_name:            ENV["GMAIL_EMAIL"],
+#   password:             ENV["GMAIL_PASSWORD"],
+#   authentication:       'plain',
+#   enable_starttls_auto: true  }
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  domain:         "heroku.com",
-  address:        "smtp.sendgrid.net",
-  port:            587,
-  authentication: :plain,
-  user_name:      'apikey',
-  password:       ENV['SENDGRID_API_KEY']
-}
+# ActionMailer::Base.smtp_settings = {
+#   domain:         "arturoestnav.com",
+#   address:        "smtp.sendgrid.net",
+#   port:            587,
+#   authentication: :plain,
+#   user_name:      'apikey',
+#   password:       ENV['SENDGRID_API_KEY']
+# }
+
