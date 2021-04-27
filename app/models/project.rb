9ab6e83ffa97c3e50e0356f2acb2a3d8ba0510b7
Arturo Estrada <arturoestradanav@gmail.com>
2021-04-27 14:10:02 -0500

    Update model association and seedfile to accommodate ImageTag tables

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   validates :name, :description, :url, presence: true
+  has_many :project_image_tags
   has_many :image_tags, through: :project_image_tags
-  has_many :project_image_tags, dependent: :destroy
-  # has_many_attached :screenshots
-  # has_one_attached :mobile_screenshot
+  has_many_attached :screenshots
+  has_one_attached :mobile_screenshot
 end
