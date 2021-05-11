bea61ada440307e83cd3fddda83854d14d46e967
Arturo Estrada <arturoestradanav@gmail.com>
2021-05-10 22:47:45 -0500

    Update dark mode form colors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -39,6 +39,7 @@ import 'controllers'
 // Carousel logic
 import { Application } from "stimulus"
 import Carousel from "stimulus-carousel"
+import 'swiper/swiper-bundle.min.css'
 
 const application = Application.start()
 application.register("carousel", Carousel)
