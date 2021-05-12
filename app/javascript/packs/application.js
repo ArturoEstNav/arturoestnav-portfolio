11814b31d4df3cf7fcd43e052d66bb21ec3a2b95
Arturo Estrada <arturoestradanav@gmail.com>
2021-05-11 20:13:03 -0500

    Add vanilla JS responsive carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,3 +35,45 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 import 'controllers'
+// MESSY CAROUSEL LOGIC
+
+const setCarousel = (projectList) => {
+  // add a class to 1st element in the array of carousel
+  let track = document.querySelector(`#${projectList} .custom-carousel-track`);
+  let slides = Array.from(track.children[0].children);
+  let prevButton = document.querySelector(`#${projectList} .custom-carousel-header .custom-controls`).children[0];
+  let nextButton = document.querySelector(`#${projectList} .custom-carousel-header .custom-controls`).children[1];
+  let slideSize = (slides[0].getBoundingClientRect().width + 24);
+  slides[0].className += " current";
+
+  // align all the elements of the array in a linear fashion
+  let setSlidePosition = (slide, index) => {
+    slides[index].style.left = slideSize * index + "px"
+  }
+
+  slides.forEach(setSlidePosition)
+
+  const moveToSlide = (track, currentSlide, targetSlide) => {
+    // move to next offer
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.className = "custom-carousel-slide"
+    targetSlide.className += " current";
+  }
+
+  prevButton.addEventListener('click', e => {
+    let currentSlide = track.querySelector('.current');
+    let prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide);
+  })
+
+  nextButton.addEventListener('click', e => {
+    let currentSlide = track.querySelector('.current');
+    let nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+    // Make sure that it cant get off limits
+  })
+}
+
+setCarousel('project-carousel');
